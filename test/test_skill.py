917f93d6e2e447adcd923070927c14eaaf8ecbb8
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-06-12 09:26:39 -0700

    Update version intent and messagebus client dependency (#52)

--- test/test_skill.py
+++ test/test_skill.py
@@ -37,7 +37,7 @@ import json
 from os import mkdir
 from os.path import dirname, join, exists
 from mock import Mock
-from mycroft_bus_client import Message
+from ovos_bus_client import Message
 from ovos_utils.messagebus import FakeBus
 
 
